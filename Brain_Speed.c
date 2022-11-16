f8b1d51181c156cc14b0e0918bed0e98af9fcbac
ghost code <116631167+viswaagz@users.noreply.github.com>
2022-11-16 20:18:57 +0530

    Create Brain_Speed.c

---
+++ Brain_Speed.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() 
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(x<y)
+    {
+        printf("yes");
+    
+    }
+    else
+    {
+        printf("no");
+    }
+	
+return 0;
+}
+/*
+Problem
+In ChefLand, human brain speed is measured in bits per second (bps). Chef has a threshold limit of XX bits per second above which his calculations are prone to errors. If Chef is currently working at YY bits per second, is he prone to errors?
+
+If Chef is prone to errors print YES, otherwise print NO.
+
+Input Format
+The only line of input contains two space separated integers XX and YY — the threshold limit and the rate at which Chef is currently working at.
+
+Output Format
+If Chef is prone to errors print YES, otherwise print NO.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings yes, Yes, yEs, and YES will all be treated as identical).
+input 7 9
+output YES
+*/
